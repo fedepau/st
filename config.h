54a911674c6115f3837260a872d3e579358aa4c5
Carlo Federico Pauletti <cpaulett@sissa.it>
2022-09-20 16:53:49 +0200

    minor gruvbox bg change

--- config.h
+++ config.h
@@ -146,6 +146,7 @@ static const char *colorname[] = {
  * foreground, background, cursor
  */
 unsigned int defaultfg = 15;
+/* unsigned int defaultbg = 0; */
 unsigned int defaultbg = 16;
 unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
